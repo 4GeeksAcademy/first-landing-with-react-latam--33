bb8a85274bb5486dc7d0cfb0d08c090da784a70c
Gustavo L <95352419+G-code7@users.noreply.github.com>
2024-07-30 03:12:20 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,21 +6,42 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+		  {/* Barra de navegación */}
+		  <nav>
+			<ul>
+			  <li>Home</li>
+			  <li>About</li>
+			  <li>Services</li>
+			  <li>Contact</li>
+			</ul>
+		  </nav>
+	
+		  {/* Encabezado con llamada a la acción */}
+		  <header style={{ textAlign: 'center', padding: '50px' }}>
+			<h1>A Warm Welcome!</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+			<button>Call to action!</button>
+		  </header>
+	
+		  {/* Sección de tarjetas */}
+		  <div style={{ display: 'flex', justifyContent: 'space-around', padding: '20px' }}>
+			{[1, 2, 3, 4].map((index) => (
+			  <div key={index} style={{ width: '23%' }}>
+				<img src={`https://via.placeholder.com/500x325`} alt="Placeholder" style={{ width: '100%' }} />
+				<h3>Card title</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				<button>Find Out More!</button>
+			  </div>
+			))}
+		  </div>
+	
+		  {/* Pie de página */}
+		  <footer style={{ textAlign: 'center', padding: '20px' }}>
+			<p>Copyright © Your Website 2024</p>
+		  </footer>
 		</div>
-	);
-};
+	  );
+	};
 
 export default Home;
