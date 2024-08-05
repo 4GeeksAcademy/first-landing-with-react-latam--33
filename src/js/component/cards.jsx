3a2b0238ba967bff513b6a39175063666f151dac
Gustavo L <95352419+G-code7@users.noreply.github.com>
2024-08-05 04:56:33 +0000

    finish landing page

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -1,20 +1,27 @@
-import React from "react";
-
-/* Sección de tarjetas */
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Cards = () => {
-    return(
-        <div className = "d-flex flex-wrap p-3" style={{ display: 'flex', justifyContent: 'space-around'}}>
-			{[1, 2, 3, 4].map((index) => (
-			  <div className="p-2" key={index} style={{ width: '320px' }}>
-				<img src={`https://via.placeholder.com/500x325`} alt="Placeholder" style={{ width: '100%' }} />
-				<h3>Card title</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-				<button>Find Out More!</button>
-			  </div>
-			))}
-		  </div>
-    )
+    return (
+        <Container className="py-5">
+            <Row className="justify-content-center" >
+                {[1, 2, 3, 4].map((index) => (
+                    <Col xs={12} sm={6} md={4} lg={3} key={index} className="mb-4">
+                        <div className="border border-light pb-2 text-center h-100">
+                            <img src={`https://via.placeholder.com/500x325`} alt="Placeholder" className="img-fluid" style={{ border: 'none' }} />
+                            <h3 className="py-3">Card title</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <div className="d-flex justify-content-center pb-2">
+                                <button className="btn btn-primary">Find Out More!</button>
+                            </div>
+                        </div>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
 }
 
 export default Cards;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Nvbar from "./navbar.jsx"
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
 import Jumbotron from "./jumbotron.jsx";
 import Cards from './cards.jsx';
+import Footer from './footer.jsx'
 
->>>>>>> main
 //include images into your bundle
+
 import rigoImage from "../../img/rigo-baby.jpg";
 
 
@@ -15,30 +13,17 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	return (
 		<div>
-		  {/* Barra de navegación */}
-		  <Nvbar></Nvbar>
-    
-		  {/* Hero Section */}
+		  	{/* Barra de navegación */}
+			<Nvbar></Nvbar>
+
+			{/* Hero Section */}
 			<Jumbotron></Jumbotron>
 
-    {/* Sección de tarjetas */}
-      <Cards></Cards>
-
-// 			<div style={{ display: 'flex', justifyContent: 'space-around', padding: '20px' }}>
-// 				{[1, 2, 3, 4].map((index) => (
-// 					<div key={index} style={{ width: '23%' }}>
-// 						<img src={`https://via.placeholder.com/500x325`} alt="Placeholder" style={{ width: '100%' }} />
-// 						<h3>Card title</h3>
-// 						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-// 						<button>Find Out More!</button>
-// 					</div>
-// 				))}
-// 			</div>
+			{/* Sección de tarjetas */}
+			<Cards></Cards>
 
 			{/* Pie de página */}
-			<footer style={{ textAlign: 'center', padding: '20px' }}>
-				<p>Copyright © Your Website 2024</p>
-			</footer>
+			<Footer></Footer>
 		</div>
 	);
 };
