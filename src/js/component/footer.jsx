33cb90aed2208032cdb3427453377dc624428bbf
Kevin Cristhian Villca Torrico <kevtorricov@gmail.com>
2024-08-16 22:56:15 +0000

    Add a div container to cards in home component, and eliminate fixed botton in the className of footer

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 
 function Footer() {
     return (
-        <footer className="bg-dark text-white fixed-bottom">
+        <footer className="bg-dark text-white">
             <Container className="text-center py-3">
                 <p className="mb-0">Copyright © Your Website 2024</p>
             </Container>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,52 +1,33 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Nvbar from "./navbar.jsx"
+import Jumbotron from "./jumbotron.jsx";
 import Cards from './cards.jsx';
+import Footer from './footer.jsx'
 
 //include images into your bundle
+
 import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
 	return (
 		<div>
-		  {/* Barra de navegación */}
-		  	<Navbar expand="lg" className="bg-dark text-white" >
-				<Container>
-					<Navbar.Brand href="#home" className="text-white">Start Bootsrap</Navbar.Brand>
-					<Navbar.Toggle aria-controls="basic-navbar-nav" className="text-white" />
-						<Navbar.Collapse id="basic-navbar-nav" className="lg-d-flex justify-content-end ">
-							<Nav>
-								<Nav.Link href="#home" className="text-white">Home</Nav.Link>
-								<Nav.Link href="#link" className="text-white">About</Nav.Link>
-								<Nav.Link href="#link" className="text-white">Services</Nav.Link>
-								<Nav.Link href="#link" className="text-white">Contact</Nav.Link>
-							</Nav>
-						</Navbar.Collapse>
-				</Container>
-			</Navbar>
-  
+		  	{/* Barra de navegación */}
+			<Nvbar></Nvbar>
 
-	
-		  {/* Encabezado con llamada a la acción */}
-		  <header style={{ textAlign: 'center', padding: '50px' }}>
-			<h1>A Warm Welcome!</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-			<button>Call to action!</button>
-		  </header>
-	
-		  {/* Sección de tarjetas */}
-		  <div>
-		  	<Cards></Cards>
-		  </div>
-		  
-		  {/* Pie de página */}
-		  <footer style={{ textAlign: 'center', padding: '20px' }}>
-			<p>Copyright © Your Website 2024</p>
-		  </footer>
+			{/* Hero Section */}
+			<Jumbotron></Jumbotron>
+
+			{/* Sección de tarjetas */}
+			<div>
+				<Cards></Cards>
+			</div>
+			
+			{/* Pie de página */}
+			<Footer></Footer>
 		</div>
-	  );
-	};
+	);
+};
 
 export default Home;
